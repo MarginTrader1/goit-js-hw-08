ba29a3d7eb0d64380f4d5cad98f0173fbee434e5
MarginTrader1 <66798324+MarginTrader1@users.noreply.github.com>
2023-05-21 20:02:17 +0000

    Deploying to gh-pages from @ MarginTrader1/goit-js-hw-08@8fbcda8fc2e3e5382af434d95085971ea1aa9344 🚀

---
+++ 03-feedback.9e5750d2.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("submit",(function(e){e.preventDefault();const{elements:{email:t,password:r}}=e.currentTarget;if(""===t.value||""===r.value)return alert("Все поля должны быть заполнены");localStorage.setItem("videoplayer-current-time",data.seconds)}));
+//# sourceMappingURL=03-feedback.9e5750d2.js.map
