66247e1aaa256004a79fcf991576b29ac28a0d92
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-05-21 22:00:12 +0300

    сделал 2 задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,35 @@
+// ипорт плеера
 import Player from "@vimeo/player";
 
+// импорт библиотеки лодаш
+import throttle from "lodash.throttle";
+
+// получаем ссылку на айфрейм
 const iframe = document.querySelector("iframe");
-    const player = new Vimeo.Player(iframe);
 
-    player.on("play", function() {
-        console.log("played the video!");
-    });
+// инициализируем плеер
+const player = new Player(iframe);
 
-    player.getVideoTitle().then(function(title) {
-        console.log("title:", title);
-    });
+player.on("play", function() {
+    console.log("played the video!");
+});
+
+// добавляем время в хранилище
+player.on("timeupdate", throttle((data) => {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
+
+// забираем время из хранилища
+const savedTime = localStorage.getItem('videoplayer-current-time');
+console.log(savedTime);
+
+// время при перезагрузке
+player.setCurrentTime(savedTime);
+
+
+
+
+
+
+
