b8e2e3e6f44ad7a6f5c6dc6cc89fbe0e83deb6f9
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-05-21 15:59:17 +0300

    add galery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryContainer = document.querySelector('.gallery'); 
+
+const images = galleryItems.map(({preview, original, description}) => ` 
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`).join(''); 
+ 
+gallaryContainer.insertAdjacentHTML('beforeend', images);  
+ 
+const gallaryLightboxContainer = new SimpleLightbox('.gallery a', { 
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 
+});
