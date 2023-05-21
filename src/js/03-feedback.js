8fbcda8fc2e3e5382af434d95085971ea1aa9344
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-05-21 23:01:45 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const formElem = document.querySelector(".feedback-form");
 
+
+formElem.addEventListener(`submit`, handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if (email.value === "" || password.value === "") {
+        return alert(`Все поля должны быть заполнены`);
+      }
+      
+      localStorage.setItem("videoplayer-current-time", data.seconds);
+    
+    //   console.log(`email: ${email.value}, Password: ${password.value}`);
+      
+
+}
