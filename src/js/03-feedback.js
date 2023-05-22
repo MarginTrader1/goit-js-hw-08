d6b344b383859a14cc0df2661345008c507e8198
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-05-23 00:31:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,52 @@
 import { save, load } from "./04-storage.js";
 
-const formElem = document.querySelector(".feedback-form");
+// импорт библиотеки лодаш
+import throttle from "lodash.throttle";
 
-formElem.addEventListener(`submit`, handleSubmit);
+const formElement = document.querySelector(".feedback-form");
 
+formElement.addEventListener(`input`, throttle(handleInput, 500));
+formElement.addEventListener(`submit`, handleSubmit);
+
+const STORAGE_KEY = "feedback-form-state";
+
+// функция заполнения формы
+function handleInput(event) {
+
+  const email = event.target.email.value;
+  const message = event.target.message.value;
+  
+  // добавляем объект в хранилище
+  localStorage.setItem(STORAGE_KEY, 
+  JSON.stringify(createStorageObject(email, message)));
+}
+
+// функция отправки формы
 function handleSubmit(event) {
-    event.preventDefault();
-    const {
-        elements: { email, password }
-      } = event.currentTarget;
+  event.preventDefault();
 
-      if (email.value === "" || password.value === "") {
+  const email = event.target.email.value;
+  const message = event.target.message.value;
+    
+    // проверка полей
+      if (email === "" || message === "") {
         return alert(`Все поля должны быть заполнены`);
       }
-      
-      localStorage.setItem("videoplayer-current-time", data.seconds);
-    
-    //   console.log(`email: ${email.value}, Password: ${password.value}`);
-      
-
+      event.target.reset();
+      localStorage.removeItem(STORAGE_KEY);
 }
+
+// забираем объект из хранилища 
+const dataStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+// устанавливаем данные в форму при перезагрузке 
+formElement.email.value = dataStorage.email;
+formElement.message.value = dataStorage.message;
+
+// функция для создания объекта
+function createStorageObject (email, message){
+  return {
+    email: `${email}`,
+    message: `${message}`,
+  };
+};
