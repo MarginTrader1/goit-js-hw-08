c78603e6b7f4b937a92ef6ad76f81c8a4b6acb5c
MarginTrader1 <66798324+MarginTrader1@users.noreply.github.com>
2023-05-22 21:31:48 +0000

    Deploying to gh-pages from @ MarginTrader1/goit-js-hw-08@d6b344b383859a14cc0df2661345008c507e8198 🚀

--- 03-feedback.9e5750d2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("submit",(function(e){e.preventDefault();const{elements:{email:t,password:r}}=e.currentTarget;if(""===t.value||""===r.value)return alert("Все поля должны быть заполнены");localStorage.setItem("videoplayer-current-time",data.seconds)}));
-//# sourceMappingURL=03-feedback.9e5750d2.js.map
--- 03-feedback.c809802a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements,r=t.email,a=t.password;if(""===r.value||""===a.value)return alert("Все поля должны быть заполнены");localStorage.setItem("videoplayer-current-time",data.seconds)}));
-//# sourceMappingURL=03-feedback.c809802a.js.map
